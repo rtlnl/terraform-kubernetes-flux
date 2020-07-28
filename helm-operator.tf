85d4cd5cf20f12f180c03ea74aa13643951c95d3
Davide Berdin <davideberdin@gmail.com>
2020-07-28 10:37:00 +0200

    Update helm-operator.tf

--- helm-operator.tf
+++ helm-operator.tf
@@ -1,7 +1,21 @@
+  
+data "template_file" "crds" {
+  template = file("${path.module}/templates/crds.yaml")
+}
+
 resource "null_resource" "crds" {
 
+  triggers = {
+    manifest_sha1 = sha1("${data.template_file.crds.rendered}")
+  }
+
   provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml"
+    command = "kubectl apply -f - <<EOF\n${data.template_file.crds.rendered}\nEOF"
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+    command = "kubectl delete -f - <<EOF\n${data.template_file.crds.rendered}\nEOF"
   }
 }
 
